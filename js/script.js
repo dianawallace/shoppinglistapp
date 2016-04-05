1432871aed74970f73c393c692df298757dfd115
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-04-05 00:14:31 +0000

    jQuery addItem

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  $('.add-item').on('click', function () {
+        var toDoItem = prompt("Type in your To Do Item");
+        if(toDoItem !== null) {
+            console.log("add this to list", toDoItem);
+            var toAppend = '<div class="row items"><div class="col-xs-2"> <label><input type="radio"></label> </div> <div class="col-xs-8 text-left">\
+                <h2>' + toDoItem + '</h2> </div> <div class="col-xs-2"> <i class="fa fa-chevron-right"></i> </div> </div>';
+            $('.todo-list').append(toAppend);
+        } else {
+            console.log("no value");
+        }
+  });
+});
+
+
