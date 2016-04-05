bf3791a30d8b25066b0f3c3a47e9dfc286efceff
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-04-05 02:16:06 +0000

    remove item

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ $(document).ready(function() {
         var toDoItem = prompt("Type in your To Do Item");
         if(toDoItem !== null) {
             console.log("add this to list", toDoItem);
-            var toAppend = '<div class="row items"><div class="col-xs-2"> <label><input type="radio"></label> </div> <div class="col-xs-8 text-left">\
+            var toAppend = '<div class="row items"><div class="col-xs-2"> <label><input type="radio" class="delete"></label> </div> <div class="col-xs-8 text-left">\
                 <h2>' + toDoItem + '</h2> </div> <div class="col-xs-2"> <i class="fa fa-chevron-right"></i> </div> </div>';
             $('.todo-list').append(toAppend);
         } else {
             console.log("no value");
         }
   });
+  $('.todo-list').on('click','.delete',function() {
+    $(this).closest('.row').remove();
+   
+});
+  
 });
 
 
